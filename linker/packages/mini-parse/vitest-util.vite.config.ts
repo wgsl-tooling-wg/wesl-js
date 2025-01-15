1db74263a643c53e910e65a4e39fe5243c336247
lee mighdoll <lee@underneath.ca>
2025-01-15 09:13:47 -0800

    add trimmedMatch to a shared package: mini-parse/vitest-util

--- linker/packages/mini-parse/vitest-util.vite.config.ts
+++ linker/packages/mini-parse/vitest-util.vite.config.ts
@@ -6,9 +6,9 @@ import { resolve } from "path";
 const config = baseViteConfig();
 config.build.emptyOutDir = false;
 const lib = config.build.lib as LibraryOptions;
-lib.name = "mini-parse-test-util";
+lib.name = "mini-parse-vitest-util";
 lib.formats = ["es"];
-lib.entry = [resolve(__dirname, "./src/test-util/index.ts")];
-lib.fileName = "testUtil";
+lib.entry = [resolve(__dirname, "./src/vitest-util/index.ts")];
+lib.fileName = "vitestUtil";
 
 export default defineConfig(config);
