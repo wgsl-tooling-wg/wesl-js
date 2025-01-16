aca831e7482a7f20b8518a7befe21290a59f9a25
lee mighdoll <lee@underneath.ca>
2025-01-16 13:31:15 -0800

    enable binding struct transform for bulk tests

--- linker/packages/bulk-test/src/test/parallelTest.test.ts
+++ linker/packages/bulk-test/src/test/parallelTest.test.ts
@@ -1,7 +1,7 @@
 import { expectNoLog } from "mini-parse/test-util";
 import fs from "node:fs/promises";
 import { expect, test } from "vitest";
-import { link, noSuffix } from "wgsl-linker";
+import { enableBindingStructs, link, noSuffix } from "wgsl-linker";
 import { findBulkTestPaths } from "../findBulkTests.ts";
 
 export interface NamedPath {
@@ -11,18 +11,22 @@ export interface NamedPath {
 
 const allPaths = await findBulkTestPaths();
 
+// const somePaths = allPaths.filter((p) => p.name.includes("fragmentTextureQuad"));
+// testWgslFiles(somePaths);
 testWgslFiles(allPaths);
 
 /** test files run this to run vite tests for all wgsl files in their partition.
  * Each test simple runs the parser to validate that it runs w/o error.
  * @param fileNames wgsl file paths to load and parse */
 export function testWgslFiles(namedPaths: NamedPath[]) {
+  const config = enableBindingStructs();
+
   namedPaths.forEach(({ name, filePath }) => {
     const shortPath = "./" + name;
     test(name, async () => {
       const text = await fs.readFile(filePath, { encoding: "utf8" });
       const result = expectNoLog(() =>
-        link({ [shortPath]: text }, noSuffix(name)),
+        link({ [shortPath]: text }, noSuffix(name), {}, [], config),
       );
       expect(result.dest).eq(text);
     });
