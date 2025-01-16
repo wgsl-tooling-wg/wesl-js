13600de58631103c49b262081812aa7fa1deb75b
lee mighdoll <lee@underneath.ca>
2025-01-16 14:56:55 -0800

    tell eslint that scripts and config files can have console.log in 'em

--- linker/eslint.config.mjs
+++ linker/eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,4 +11,12 @@ export default tseslint.config(
     "@typescript-eslint/no-unused-vars": "warn",
    } 
   },
+  {
+    files: ["*.js", "**/scripts/*.m?[jt]s"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  }
 );
