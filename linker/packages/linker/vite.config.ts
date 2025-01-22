255e1d358cf33d876372ccab0467294378694457
lee mighdoll <lee@underneath.ca>
2025-01-21 20:07:23 -0800

    rm debug

--- linker/packages/linker/vite.config.ts
+++ linker/packages/linker/vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import { baseViteConfig } from "./base.vite.config.js";
-// import { visualizer } from "rollup-plugin-visualizer";
 
 const config = baseViteConfig();
 export default defineConfig(config);
