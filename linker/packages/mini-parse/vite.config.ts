c7026edb9e672992d06139f5df31df9ce520eac8
lee mighdoll <lee@underneath.ca>
2025-01-25 20:47:17 -0800

    fix build:all by removing brotli and adding vite dep back into mini-parse

--- linker/packages/mini-parse/vite.config.ts
+++ linker/packages/mini-parse/vite.config.ts
@@ -3,7 +3,7 @@ import { LibraryOptions, defineConfig } from "vite";
 import { baseViteConfig } from "./base.vite.config.js";
 
 const config = baseViteConfig();
-config.test = { setupFiles: "./src/test/TestSetup.ts" };
+// config.test = { setupFiles: "./src/test/TestSetup.ts" };
 (config.build.lib as LibraryOptions).formats = ["es", "cjs"];
 (config.build.lib as LibraryOptions).name = "mini-parse";
 
