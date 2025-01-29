193f39c974e548f73037fa365ddbb29583ff20a3
lee mighdoll <lee@underneath.ca>
2025-01-28 21:55:35 -0800

    expose reflection plugin types in types.d.ts

--- linker/packages/plugin-test/main.ts
+++ linker/packages/plugin-test/main.ts
@@ -1,3 +1,4 @@
-import { layouts } from "./shaders/app.wesl?reflect";
+/// <reference types="wesl-plugin" />
+import { layoutEntries } from "./shaders/app.wesl?reflect";
 document.getElementById("app")!.innerHTML =
-  "Plugin Test: " + [...Object.keys(layouts)].toString();
+  "Plugin Test: " + [...Object.keys(layoutEntries)].toString();
