8a2d82c6c0dd4c66220cd8d656ea125912ef4a5e
lee mighdoll <lee@underneath.ca>
2025-02-01 09:43:34 -0800

    type layoutFunctions api

--- linker/packages/plugin/src/types.d.ts
+++ linker/packages/plugin/src/types.d.ts
@@ -1,5 +1,5 @@
 /** @hidden */
 declare module "*?reflect" {
   export const layoutEntries: Record<string, GPUBindGroupLayoutEntry[]>;
-  export const layoutFunctions: Record<string, any>;
+  export const layoutFunctions: Record<string, (device:GPUDevice) => GPUBindGroupLayout>;
 }
