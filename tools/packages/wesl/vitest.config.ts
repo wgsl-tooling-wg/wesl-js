784bf26bdd3b455933d6ae8d2342379ec7e56c4c
lee mighdoll <lee@underneath.ca>
2025-02-11 21:51:25 -0800

    rm vite coddling for testsuite, no longer needed

--- tools/packages/wesl/vitest.config.ts
+++ tools/packages/wesl/vitest.config.ts
@@ -3,15 +3,9 @@ import { mergeConfig } from "vite";
 import { baseViteConfig } from "./base.vite.config.js";
 // import { visualizer } from "rollup-plugin-visualizer";
 
-const aliasPath = new URL("../../../wesl-testsuite", import.meta.url).pathname;
-
 const merged = mergeConfig(baseViteConfig(), {
   test: {
     setupFiles: "./src/test/TestSetup.ts",
-  },
-  resolve: {
-    // so vitest reruns when tests change
-    alias: { "wesl-testsuite": aliasPath },
   },
 });
 
