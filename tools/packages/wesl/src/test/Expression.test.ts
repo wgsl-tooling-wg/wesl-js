61c9d8106e4eb2e25f19f789d1d667cd65c8d5c9
Stefnotch <brandmairstefan@gmail.com>
2025-02-15 23:34:48 +0100

    Rename file

--- tools/packages/wesl/src/test/Expression.test.ts
+++ tools/packages/wesl/src/test/Expression.test.ts
@@ -1,6 +1,6 @@
 import { eof, seq } from "mini-parse";
 import { expect, test } from "vitest";
-import { expression } from "../parse/WESLGrammar.ts";
+import { expression } from "../parse/WeslGrammar1.ts";
 import { testAppParse } from "./TestUtil.ts";
 
 test("parse number", () => {
