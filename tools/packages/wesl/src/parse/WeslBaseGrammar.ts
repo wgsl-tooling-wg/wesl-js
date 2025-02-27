b66dd9dfb0b496465de2aced2d2ec3e0fb943ade
lee mighdoll <lee@underneath.ca>
2025-02-27 13:37:36 -0800

    interim fix and test case for parsing package::foo::bar()

--- tools/packages/wesl/src/parse/WeslBaseGrammar.ts
+++ tools/packages/wesl/src/parse/WeslBaseGrammar.ts
@@ -1,6 +1,9 @@
-import { kind, withSepPlus } from "mini-parse";
+import { kind, or, withSepPlus } from "mini-parse";
 import { WeslTokenKind } from "./WeslStream";
 
 export const word = kind<WeslTokenKind>("word");
-export const qualified_ident = withSepPlus("::", word);
+export const keyword = kind<WeslTokenKind>("keyword");
+
+// or(word, keyword) is imprecise here. But package::foo::bar is a qualifed ident.. TODO consider a better fix. 
+export const qualified_ident = withSepPlus("::", or(word, keyword)); 
 export const number = kind<WeslTokenKind>("number");
