9743180cf0ccd9c9b1230a773db9760c7dd82f29
lee mighdoll <lee@underneath.ca>
2025-02-28 22:17:18 -0600

    combine conditions parsing tests into one file

--- tools/packages/wesl/src/test/ParseConditions.test.ts
+++ tools/packages/wesl/src/test/ParseConditions.test.ts
@@ -2,6 +2,19 @@ import { expect, test } from "vitest";
 import { astToString } from "../debug/ASTtoString.ts";
 import { parseTest } from "./TestUtil.ts";
 
+test("parse complex condition", () => {
+  const ast = parseTest("@if(true || (!foo&&!!false) )\nfn a() {}");
+  expect(astToString(ast.moduleElem)).toMatchInlineSnapshot(`
+    "module
+      fn a() @if
+        attribute @if(true || (!foo && !!false))
+        text '
+    fn '
+        decl %a
+        text '() {}'"
+  `);
+});
+
 test("@if(false) enable f16", () => {
   const src = `
     @if(false) enable f16;
