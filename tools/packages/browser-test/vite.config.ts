2e09845bdda3c802156c3660b478fbb34bee1391
Stefnotch <brandmairstefan@gmail.com>
2025-03-08 22:34:08 +0100

    Add a simple browser testing project

---
+++ tools/packages/browser-test/vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest/config" />
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { UserConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const thisPath = fileURLToPath(import.meta.url);
+const weslToml = path.join(path.dirname(thisPath), "wesl.toml");
+
+const config: UserConfig = {
+  plugins: [tsconfigPaths()],
+};
+
+export default config;
