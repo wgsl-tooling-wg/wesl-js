09a300b8aca9846483a262eb85ea98df0d6b30cf
lee mighdoll <lee@underneath.ca>
2025-03-13 16:40:36 -0700

    support loading libs in ?static plugin

--- tools/packages/wesl-plugin/src/extensions/StaticExtension.ts
+++ tools/packages/wesl-plugin/src/extensions/StaticExtension.ts
@@ -1,4 +1,6 @@
+import { resolve } from "import-meta-resolve";
 import path from "node:path";
+import url from "node:url";
 import { link, noSuffix } from "wesl";
 import { PluginExtension, PluginExtensionApi } from "../PluginExtension.ts";
 
@@ -15,20 +17,31 @@ async function emitStaticJs(
   const { resolvedWeslRoot, toml, tomlDir } = await api.weslToml();
   const { dependencies = [] } = toml;
 
+  // resolve relative to the file with the import statement
+  const parentModule = url.pathToFileURL(baseId).toString();
+  const libFileUrls = dependencies.map(d => resolve(d, parentModule));
+
+  // load the lib modules
+  const futureLibs = libFileUrls.map(f => import(f));
+  const libModules = await Promise.all(futureLibs);
+  const libs = libModules.map(m => m.default);
+
+  // find weslSrc and rootModule
   const weslSrc = await api.weslSrc();
   const rootModule = await api.weslMain(baseId);
   const rootModuleName = noSuffix(rootModule);
 
+  // find weslRoot
   const tomlRelative = path.relative(tomlDir, resolvedWeslRoot);
   const debugWeslRoot = tomlRelative.replaceAll(path.sep, "/");
 
-  const result = await link({ weslSrc, rootModuleName, debugWeslRoot }); // TODO handle libs
+  const result = await link({ weslSrc, rootModuleName, debugWeslRoot, libs });
   const wgsl = result.dest;
 
   const src = `
     export const wgsl = \`${wgsl}\`;
     export default wgsl;
-    `;
+  `;
 
 
   return src;
--- tools/packages/wesl-plugin/test/staticExtension/dir/StaticExtension.test.ts
+++ tools/packages/wesl-plugin/test/staticExtension/dir/StaticExtension.test.ts
@@ -5,8 +5,45 @@ import wgsl from "../shaders/foo/app.wesl?static";
 test("verify ?static", async () => {
   expectTypeOf(wgsl).toMatchTypeOf<string>();
   expect(wgsl).toMatchInlineSnapshot(`
-    "fn main() {
-       let a = 1; 
+    "
+
+    fn main() {
+       let a = pcg_2u_3f(vec2u(1, 2)); 
+    }
+
+    fn pcg_2u_3f(pos: vec2u) -> vec3f {
+        let seed = mix2to3(pos);
+        let random = pcg_3u_3u(seed);
+        let normalized = ldexp(vec3f(random), vec3(-32));
+        return vec3f(normalized);
+    }
+
+    fn mix2to3(p: vec2u) -> vec3u {
+        let seed = vec3u(
+            p.x,
+            p.x ^ p.y,
+            p.x + p.y,
+        );
+        return seed;
+    }
+
+    fn pcg_3u_3u(seed: vec3u) -> vec3u {
+        var v = seed * 1664525u + 1013904223u;
+
+        v = mixing(v);
+        v ^= v >> vec3(16u);
+        v = mixing(v);
+
+        return v;
+    }
+
+    fn mixing(v: vec3u) -> vec3u {
+        var m: vec3u = v;
+        m.x += v.y * v.z;
+        m.y += v.z * v.x;
+        m.z += v.x * v.y;
+
+        return m;
     }"
   `);
 });
