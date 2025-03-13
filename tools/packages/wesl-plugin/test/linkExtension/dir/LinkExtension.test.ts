44c025d46978f5a2ff84d71692a0b06e01b4d543
lee mighdoll <lee@underneath.ca>
2025-03-12 21:43:37 -0700

    (partial) LinkExtension test uses more directories

--- tools/packages/wesl-plugin/test/linkExtension/dir/LinkExtension.test.ts
+++ tools/packages/wesl-plugin/test/linkExtension/dir/LinkExtension.test.ts
@@ -1,8 +1,7 @@
 /// <reference types="wesl-plugin/suffixes" />
-import { dlog } from "berry-pretty";
 import { expect, expectTypeOf, test } from "vitest";
 import { LinkParams } from "wesl";
-import linkParams from "./shaders/app.wesl?link";
+import linkParams from "../shaders/foo/app.wesl?link";
 
 test("verify ?link", async () => {
   expectTypeOf(linkParams).toMatchTypeOf<LinkParams>();
@@ -11,9 +10,7 @@ test("verify ?link", async () => {
     linkParams as LinkParams;
   expect(rootModuleName).toMatchInlineSnapshot(`"app"`);
 
-  dlog("fixme", { debugWeslRoot });
-  // TODO this result can't be right... weslRoot should be relative to the tomlDir probably.
-  // expect(debugWeslRoot).toMatchInlineSnapshot(`"packages/wesl-plugin/test/linkExtension/shaders"`);
+  expect(debugWeslRoot).toMatchInlineSnapshot(`"shaders/foo"`);
 
   expect(weslSrc).toMatchInlineSnapshot(`
     {
