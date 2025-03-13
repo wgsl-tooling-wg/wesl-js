9ae17333362dc5811be0597f905c6bc35b2c0445
lee mighdoll <lee@underneath.ca>
2025-03-13 11:16:43 -0700

    verify no toml error message

--- tools/packages/wesl-plugin/test/linkExtensionNoToml/LinkExtensionNoToml.test.ts
+++ tools/packages/wesl-plugin/test/linkExtensionNoToml/LinkExtensionNoToml.test.ts
@@ -4,15 +4,17 @@ import { fileURLToPath } from "node:url";
 import util from "node:util";
 import { expect, test } from "vitest";
 const exec = util.promisify((process as any).exec); // not sure why @types/node for child_process.exec is wrong (nodeExec vs exec)
+import { defaultTomlMessage } from "../../../wesl-plugin/src/weslPlugin.js";
 
 const testDir = dirname(fileURLToPath(import.meta.url));
 
 test("verify ?link", async () => {
   // build test program
-  await exec(`pnpm vite build`, {
+  const buildResult = await exec(`pnpm vite build`, {
     cwd: testDir,
   });
   const outFile = path.join("dist", "noTomlMain.cjs");
+  expect(buildResult.stdout).includes(defaultTomlMessage);
 
   // run a test program that logs the ?link output to the console for verification
   const result = await exec(`pnpm node ${outFile}`, {
