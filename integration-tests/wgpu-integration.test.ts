98735d40680fd0dc0468576b80e4ce10e4f121fc
Stefnotch <brandmairstefan@gmail.com>
2025-03-28 14:11:34 +0100

    Simplify wgpu integration test

--- integration-tests/wgpu-integration.test.ts
+++ integration-tests/wgpu-integration.test.ts
@@ -10,18 +10,6 @@ Deno.test.ignore("Pass WESL shader to WebGPU", async () => {
 
   const module = device.createShaderModule({
     code: `
-      @vertex fn vs(
-        @builtin(vertex_index) vertexIndex : u32
-      ) -> @builtin(position) vec4f {
-        let pos = array(
-          vec2f( 0.0,  0.5),
-          vec2f(-0.5, -0.5),
-          vec2f( 0.5, -0.5),
-        );
-  
-        return vec4f(pos[vertexIndex], 0.0, 1.0);
-      }
-  
       @fragment fn fs() -> @location(0) vec4f {
         return vec4f(1, 0, 0, 1);
       }
