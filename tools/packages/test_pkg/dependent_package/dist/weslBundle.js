feb0a032c6f3f19d25d06690e449afb81e0d1e8f
lee mighdoll <lee@underneath.ca>
2025-04-27 18:25:48 -0500

    npm package w/multiple exports and transitive

---
+++ tools/packages/test_pkg/dependent_package/dist/weslBundle.js
@@ -0,0 +1,11 @@
+
+export const weslBundle = {
+  "name": "dependent_package",
+  "edition": "unstable_2025_1",
+  "modules": {
+    "lib.wesl": "fn dep() { } "
+  }
+}
+
+export default weslBundle;
+  
---
+++ tools/packages/test_pkg/multi_pkg/dist/dir/nested/weslBundle.js
@@ -0,0 +1,11 @@
+
+export const weslBundle = {
+  "name": "multi_pkg",
+  "edition": "unstable_2025_1",
+  "modules": {
+    "dir/nested.wesl": "fn nest() {} "
+  }
+}
+
+export default weslBundle;
+  
