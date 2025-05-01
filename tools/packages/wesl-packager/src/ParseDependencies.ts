dac7aa352407149459f6a24b32f6101994fa0f6d
lee mighdoll <lee@underneath.ca>
2025-05-01 11:19:51 -0700

    parseDependencies() - find package refs from wesl src

--- tools/packages/wesl-packager/src/ParseDependencies.ts
+++ tools/packages/wesl-packager/src/ParseDependencies.ts
@@ -1,6 +1,12 @@
+import { resolve } from "import-meta-resolve";
+import path from "node:path";
+import { pathToFileURL } from "node:url";
+import { filterMap, parsedRegistry, parseIntoRegistry } from "wesl";
+import { bindIdents } from "../../wesl/src/BindIdents.ts";
+
 /**
- * Find the dependencies in a set of WESL files
- * (useful for packaging WESL files into a library)
+ * Find the wesl package dependencies in a set of WESL files
+ * (for packaging WESL files into a library)
  *
  * Parse the WESL files and partially bind the identifiers,
  * returning any identifiers that are not succesfully bound.
@@ -11,6 +17,50 @@
  *    . package foo, export '.' bundle, module bar
  *    . package foo, export './bar' bundle, element baz
  *    . package foo, export './bar/baz' bundle, module lib.wesl, element baz
- * To distinguish these, we look at the export entries in package foo's package.json
- *
+ * To distinguish these, we node resolve the longest path we can.
  */
+export function parseDependencies(
+  weslSrc: Record<string, string>,
+  projectDir: string,
+): string[] {
+  const registry = parsedRegistry();
+  parseIntoRegistry(weslSrc, registry);
+
+  const rootAst = Object.values(registry.modules)[0]; // TODO need a mode in bindIdents w/o rootAst
+  const bindResult = bindIdents({ registry, rootAst, accumulateUnbound: true });
+  const { unbound } = bindResult;
+  if (!unbound) return [];
+
+  const fullProjectDir = path.resolve(path.join(projectDir, "foo"));
+  const projectURL = pathToFileURL(fullProjectDir).href;
+  const deps = filterMap(unbound, mPath =>
+    unboundToDependency(mPath, projectURL),
+  );
+  const uniqueDeps = [...new Set(deps)];
+
+  return uniqueDeps;
+}
+
+/**
+ * Find the longest resolvable npm subpath from a module path.
+ *
+ * @param mPath module path, e.g. ['foo', 'bar', 'baz']
+ * @param importerURL URL of the importer, e.g. 'file:///path/to/project/foo/bar/baz.wesl' (doesn't need to be a real file)
+ * @returns longest resolvable subpath of mPath, e.g. 'foo/bar/baz' or 'foo/bar'
+ */
+function unboundToDependency(
+  mPath: string[],
+  importerURL: string,
+): string | undefined {
+  for (let i = mPath.length; i >= 0; i--) {
+    const subPath = mPath.slice(0, i).join("/");
+    try {
+      resolve(subPath, importerURL);
+      return subPath;
+    } catch {
+      // if the subPath cannot be resolved try a shorter version
+      continue;
+    }
+  }
+  return undefined;
+}
