98b7f6c5f79ec6ff3d0731cdc736bf3be11ee229
lee mighdoll <lee@underneath.ca>
2025-05-04 20:18:42 -0700

    TDD test for non-root dependency

---
+++ tools/packages/wesl-packager/src/test/ParseDependencies.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "vitest";
+import { parseDependencies } from "../ParseDependencies.ts";
+import { dlog } from "berry-pretty";
+
+const thisDir = import.meta.dirname;
+
+test("parseDepenencies finds non-root dependency", () => {
+  const srcs = {
+    main:`
+      import package::foo::bar;
+      fn main() { }
+    `,
+    foo: `
+      import dependent_package::dep;
+
+      fn bar() { 
+        dep(); // should be a package dependency for a library even though it's not referenced from root
+      }
+    `
+  };
+  const deps = parseDependencies(srcs, thisDir);
+  expect(deps).deep.equals(["dependent_package"]);
+});
