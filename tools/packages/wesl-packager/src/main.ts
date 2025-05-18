43b2dbf714a1ab2acb668ec4fc105e71a7892ff8
lee mighdoll <lee@underneath.ca>
2025-05-17 22:44:22 -0700

    verify node version > 22

--- tools/packages/wesl-packager/src/main.ts
+++ tools/packages/wesl-packager/src/main.ts
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
-import { exit } from "process";
+import { exit } from "node:process";
 import { hideBin } from "yargs/helpers";
 import { packagerCli } from "./PackagerCli.js";
 
 const rawArgs = hideBin(process.argv);
+
+const [major] = process.versions.node.split(".").map(Number);
+if (major < 22) {
+  console.error(`Please upgrade node to version 22 or higher. (The current node version is ${process.version})`);
+  exit(1);
+}
 
 packagerCli(rawArgs).catch(e => {
   console.error(e);
