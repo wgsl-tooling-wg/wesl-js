a0dfefb8a9137aa581ba67d50562112020cc88ab
lee mighdoll <lee@underneath.ca>
2025-05-20 10:03:22 -0700

    tdd test for auto in ?static

--- tools/packages/wesl-plugin/test/staticExtension/vitest.config.ts
+++ tools/packages/wesl-plugin/test/staticExtension/vitest.config.ts
@@ -1,18 +1,10 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { staticBuildExtension } from "wesl-plugin";
 import viteWesl from "wesl-plugin/vite";
 
-const thisPath = fileURLToPath(import.meta.url);
-const weslToml = path.join(path.dirname(thisPath), "wesl.toml");
-
 const config: UserConfig = {
-  plugins: [
-    tsconfigPaths(),
-    viteWesl({ weslToml, extensions: [staticBuildExtension] }),
-  ],
+  plugins: [tsconfigPaths(), viteWesl({ extensions: [staticBuildExtension] })],
 };
 
 export default config;
