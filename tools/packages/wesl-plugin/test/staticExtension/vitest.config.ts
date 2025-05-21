be043ade80ca825592e62848b8acd527587198a6
lee mighdoll <lee@underneath.ca>
2025-05-20 17:19:22 -0700

    fix issue running static extension test from root dir

--- tools/packages/wesl-plugin/test/staticExtension/vitest.config.ts
+++ tools/packages/wesl-plugin/test/staticExtension/vitest.config.ts
@@ -1,10 +1,13 @@
+import { fileURLToPath } from "node:url";
 import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { staticBuildExtension } from "wesl-plugin";
 import viteWesl from "wesl-plugin/vite";
 
+const weslToml = fileURLToPath(new URL("./wesl.toml", import.meta.url));
+
 const config: UserConfig = {
-  plugins: [tsconfigPaths(), viteWesl({ extensions: [staticBuildExtension] })],
+  plugins: [tsconfigPaths(), viteWesl({ weslToml, extensions: [staticBuildExtension] })],
 };
 
 export default config;
