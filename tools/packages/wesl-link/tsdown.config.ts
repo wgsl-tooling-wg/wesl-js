ac5a1ccb56c6768eb8762563a1f672c07731c6a2
lee mighdoll <lee@underneath.ca>
2025-05-22 17:12:51 -0700

    switch to tsdown for wesl-link build

---
+++ tools/packages/wesl-link/tsdown.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsdown";
+
+const toBin = "./bin/wesl-packager";
+
+// workaround - ignoreWatch doesn't seem to work with relative paths,
+// and loops forever unless we give a specific path
+const thisPath = import.meta.url;
+const binPath = new URL(toBin, thisPath).pathname;
+
+export default defineConfig({
+  entry: ["./src/main.ts"],
+  target: "node22",
+  clean: true,
+  platform: "node",
+  outputOptions: { dir: undefined, file: toBin },
+  external: ["wesl", "wesl-tooling", "yargs", "yargs/helpers"],
+  ignoreWatch: [binPath],
+});
