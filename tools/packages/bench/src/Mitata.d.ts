fc4a235d9fd1bbc70db86a583956c194420fb982
lee mighdoll <lee@underneath.ca>
2025-06-21 12:56:37 -0700

    module file for @mitata/counters

--- tools/packages/bench/src/Mitata.d.ts
+++ tools/packages/bench/src/Mitata.d.ts
@@ -1,3 +1,36 @@
 declare module "@mitata/counters" {
   function init(): void;
+  function deinit(): void;
+  function after(): void;
+  function before(): void;
+  function translate(batch?: number, samples?: number): CounterResults | undefined;
+
+  interface CounterResults {
+    // macOS (darwin) specific
+    l1?: {
+      miss_loads?: CounterStats | null;
+      miss_stores?: CounterStats | null;
+    };
+    cycles?: CounterStats & {
+      stalls?: CounterStats;
+    };
+    branches?: CounterStats & {
+      mispredicted?: CounterStats;
+    } | null;
+    instructions?: CounterStats & {
+      loads_and_stores?: CounterStats | null;
+    };
+
+    // Linux specific
+    cache?: CounterStats & {
+      misses?: CounterStats | null;
+    } | null;
+    _bmispred?: CounterStats | null;
+  }
+
+  interface CounterStats {
+    min: number;
+    max: number;
+    avg: number;
+  }
 }
