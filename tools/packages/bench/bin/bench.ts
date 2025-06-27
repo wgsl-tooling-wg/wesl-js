e61bf304d363946672930e55b38fff592f4c281a
lee mighdoll <lee@underneath.ca>
2025-06-26 17:41:54 -0700

    factor out LoadBenchmarks file

--- tools/packages/bench/bin/bench.ts
+++ tools/packages/bench/bin/bench.ts
@@ -1,18 +1,14 @@
-import fs from "node:fs/promises";
-import path from "node:path";
-import {
-  _linkSync,
-  link
-} from "wesl";
+import { _linkSync, link } from "wesl";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import { type BenchmarkReport, reportResults } from "../src/BenchmarkReport.ts";
 import {
-  type BenchmarkReport,
-  reportResults,
-} from "../src/BenchmarkReport.ts";
-import { type MeasureOptions, mitataBench } from "../src/mitata-util/MitataBench.ts";
+  type MeasureOptions,
+  mitataBench,
+} from "../src/mitata-util/MitataBench.ts";
 import { benchManually } from "../src/experiments/BenchManually.ts";
 import { simpleMitataBench } from "../src/experiments/VanillaMitata.ts";
+import { loadBenchmarkFiles } from "../src/LoadBenchmarks.ts";
 
 export interface BenchTest {
   name: string;
@@ -99,15 +95,17 @@ function benchOnceOnly(tests: BenchTest[]): Promise<any> {
   });
 }
 
-
 /** run the tests and report results */
-async function benchAndReport(tests: BenchTest[], baselineLink?: typeof link): Promise<void> {
+async function benchAndReport(
+  tests: BenchTest[],
+  baselineLink?: typeof link,
+): Promise<void> {
   const reports: BenchmarkReport[] = [];
 
   const secToNs = 1e9;
   const opts: MeasureOptions = {
     // inner_gc: true,
-    min_cpu_time: .1 * secToNs,
+    min_cpu_time: 0.1 * secToNs,
   } as any;
   for (const test of tests) {
     const weslSrc = Object.fromEntries(test.files.entries());
@@ -140,81 +138,6 @@ function selectVariant(variant: string): ParserVariant {
     return variant as ParserVariant;
   }
   throw new Error("NYI parser variant: " + variant);
-}
-
-async function loadBenchmarkFiles(): Promise<BenchTest[]> {
-  const examplesDir = "./src/examples";
-  const reduceBuffer = await loadBench(
-    "reduceBuffer",
-    examplesDir,
-    "./reduceBuffer.wgsl",
-  );
-  const particle = await loadBench("particle", examplesDir, "./particle.wgsl");
-  const rasterize = await loadBench(
-    "rasterize",
-    examplesDir,
-    "./rasterize_05_fine.wgsl",
-  );
-  const boat = await loadBench(
-    "unity_webgpu_0000026E5689B260",
-    examplesDir,
-    "./unity_webgpu_000002B8376A5020.fs.wgsl",
-  );
-  const imports_only = await loadBench(
-    "imports_only",
-    examplesDir,
-    "./imports_only.wgsl",
-  );
-  const bevy_deferred_lighting = await loadBench(
-    "bevy_deferred_lighting",
-    "./src/examples/bevy",
-    "./bevy_generated_deferred_lighting.wgsl",
-  );
-  const bevy_linking = await loadBench(
-    "bevy_linking",
-    "./src/examples/naga_oil_example",
-    "./pbr.wgsl",
-    [
-      "./clustered_forward.wgsl",
-      "./mesh_bindings.wgsl",
-      "./mesh_types.wgsl",
-      "./mesh_vertex_output.wgsl",
-      "./mesh_view_bindings.wgsl",
-      "./mesh_view_types.wgsl",
-      "./pbr_bindings.wgsl",
-      "./pbr_functions.wgsl",
-      "./pbr_lighting.wgsl",
-      "./pbr_types.wgsl",
-      "./shadows.wgsl",
-      "./utils.wgsl",
-    ],
-  );
-  return [
-    reduceBuffer,
-    // particle,
-    // rasterize,
-    // boat,
-    // imports_only,
-    // bevy_deferred_lighting,
-    // bevy_linking,
-  ];
-}
-
-async function loadBench(
-  name: string,
-  cwd: string,
-  mainFile: string,
-  extraFiles: string[] = [],
-): Promise<BenchTest> {
-  const files = new Map<string, string>();
-  const addFile = async (p: string) =>
-    files.set(p, await fs.readFile(path.join(cwd, p), { encoding: "utf8" }));
-
-  await addFile(mainFile);
-  for (const path of extraFiles) {
-    await addFile(path);
-  }
-  return { name, mainFile, files };
 }
 
 // function runOnce(parserVariant: ParserVariant, test: BenchTest): void {
