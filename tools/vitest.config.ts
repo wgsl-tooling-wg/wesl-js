fca7c5cc6b0bd8f3e34ea3d8acb03763c7b422aa
lee mighdoll <lee@underneath.ca>
2025-06-27 09:19:10 -0700

    try excluding _baseline again

--- tools/vitest.config.ts
+++ tools/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineProject } from "vitest/config";
 export default defineProject({
   test: {
     projects: ["packages/*", "packages/wesl-plugin/test/*"],
-    exclude: ["_baseline/**"],
+    exclude: ["packages/bench/_baseline/**/*"],
   },
 });
