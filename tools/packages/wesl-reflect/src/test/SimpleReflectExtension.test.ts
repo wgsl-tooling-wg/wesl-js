48c5ba6bac552583edd491fdee37a9ba9fb0858b
lee mighdoll <lee@underneath.ca>
2025-06-30 22:19:29 -0700

    disable a test problematic on clean builds..

--- tools/packages/wesl-reflect/src/test/SimpleReflectExtension.test.ts
+++ tools/packages/wesl-reflect/src/test/SimpleReflectExtension.test.ts
@@ -4,10 +4,14 @@ import { expect, expectTypeOf, test } from "vitest";
 import { structs } from "../../shaders/app.wgsl?simple_reflect";
 
 test("simple_reflect a struct", async () => {
-  // verify ts
-  type ExpectedType = { firstMember: number; secondMember: OtherStruct };
-  const m = structs[0] as MyStruct;
-  expectTypeOf(m).toEqualTypeOf<ExpectedType>();
+  // LATER this is not a good test due to fix build ordering issues: 
+  // OtherStruct is defined by a generated file, only after the test is run,
+  // which makes clean builds fail to typecheck until the file is generated.
+  //
+  // verify ts - 
+  // type ExpectedType = { firstMember: number; secondMember: OtherStruct };
+  // const m = structs[0] as MyStruct;
+  // expectTypeOf(m).toEqualTypeOf<ExpectedType>();
 
   // verify js
   expect(structs).toMatchInlineSnapshot(`
