1a71dbd93a6481486e18dcd21b36eaff302b1e55
lee mighdoll <lee@underneath.ca>
2025-06-30 22:31:28 -0700

    lint

--- tools/packages/wesl-reflect/src/test/SimpleReflectExtension.test.ts
+++ tools/packages/wesl-reflect/src/test/SimpleReflectExtension.test.ts
@@ -1,14 +1,14 @@
-import { expect, expectTypeOf, test } from "vitest";
+import { expect, test } from "vitest";
 
 // --- load reflected structs ---
 import { structs } from "../../shaders/app.wgsl?simple_reflect";
 
 test("simple_reflect a struct", async () => {
-  // LATER this is not a good test due to fix build ordering issues: 
+  // LATER this is not a good test due to fix build ordering issues:
   // OtherStruct is defined by a generated file, only after the test is run,
   // which makes clean builds fail to typecheck until the file is generated.
   //
-  // verify ts - 
+  // verify ts -
   // type ExpectedType = { firstMember: number; secondMember: OtherStruct };
   // const m = structs[0] as MyStruct;
   // expectTypeOf(m).toEqualTypeOf<ExpectedType>();
