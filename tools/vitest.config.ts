5d3a290c3ad5e6d561ec3d31fe297cc045ce4527
lee mighdoll <lee@underneath.ca>
2025-07-02 13:30:27 -0700

    help vitest to skip _baseline

--- tools/vitest.config.ts
+++ tools/vitest.config.ts
@@ -3,5 +3,6 @@ import { defineProject } from "vitest/config";
 export default defineProject({
   test: {
     projects: ["packages/*", "packages/wesl-plugin/test/*"],
+    exclude: ["_baseline/**"],
   },
 });
