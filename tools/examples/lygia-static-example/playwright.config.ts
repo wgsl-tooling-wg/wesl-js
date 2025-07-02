a69c9d56b3e7b2ea8e14eea2d5db9e1d142726d4
lee mighdoll <lee@underneath.ca>
2025-07-02 14:25:10 -0700

    playwright test for lygia-static

---
+++ tools/examples/lygia-static-example/playwright.config.ts
@@ -0,0 +1,62 @@
+import process from "node:process";
+import { defineConfig, devices } from "@playwright/test";
+import { testPort } from "./playwright.port.ts";
+
+const port = process.env.PLAYWRIGHT_PORT || testPort;
+
+export default defineConfig({
+  webServer: {
+    command: `pnpm run dev --port ${port}`,
+    url: `http://localhost:${port}`,
+    reuseExistingServer: !process.env.CI,
+    stdout: "pipe",
+  },
+
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: `http://localhost:${port}`,
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+});
---
+++ tools/examples/lygia-static-example/tests/lygia-static.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('linked code', async ({ page }) => {
+  await page.goto('/');
+  console.log("page loaded");
+  await page.getByText('@compute @workgroup_size(1)');
+  const src = await page.locator('#app').textContent();
+  expect(src).toMatchSnapshot('lygia-static.wgsl');
+});
