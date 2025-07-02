21294f0169cda1a9a409ae58443f0193f06dc368
lee mighdoll <lee@underneath.ca>
2025-07-02 12:42:26 -0700

    add test for wesl-rand example

---
+++ tools/examples/wesl-rand/tests/random.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('random image', async ({ page }) => {
+  await page.goto('/');
+
+  const canvas = await page.$('canvas');
+  expect(canvas).not.toBeNull();
+  const snapshot = await canvas!.screenshot();
+  expect(snapshot).toMatchSnapshot('random.png');
+});
