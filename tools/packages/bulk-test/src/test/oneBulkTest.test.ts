af732edc1ce66d4cfad537f9141890075dec6d04
Stefnotch <brandmairstefan@gmail.com>
2025-07-10 18:01:26 +0200

    Move bulk tests to shared repo

--- tools/packages/bulk-test/src/test/oneBulkTest.test.ts
+++
@@ -1,7 +0,0 @@
-import { allBulkTests } from "../findBulkTests.ts";
-import { testWgslFiles } from "../testWgslFiles.ts";
-
-const somePaths = allBulkTests.filter(p =>
-  p.name.includes("fullscreenTexturedQuad.wgsl"),
-);
-testWgslFiles(somePaths);
